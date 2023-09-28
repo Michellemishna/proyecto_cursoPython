52056e206783e661a494dc11e594e81f5935c2c5
Michelle Diaz <michellelunaby@gmail.com>
2023-09-27 23:01:06 -0600

    Exam python

---
+++ server.py
@@ -0,0 +1,38 @@
+''' Executing this function initiates the application and deployed on
+    localhost:5000.
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def send_analyzer():
+    ''' This code receives the text from the HTML interface and 
+        runs sentiment analysis over it using emotion_detector()
+        function.
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again."
+
+    system_response = (
+        f"For the given statement, the system response is 'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. The dominant emotion in {response['dominant_emotion']}"
+    )
+    return system_response
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
